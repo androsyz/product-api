6226bbbb3444561220ca6fb2b75cc227fcbf267d
Andro Syahreza <androsyahreza3@gmail.com>
2023-03-02 11:13:09 +0700

    refactor controller

--- src/controllers/product.controller.go
+++ src/controllers/product.controller.go
@@ -1,19 +1,22 @@
 package controllers
 
 import (
-	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/androsyahreza/product-api/src/models"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
 func Index(c *gin.Context) {
 	var products []models.Product
+	err := models.DB.Find(&products).Error
 
-	models.DB.Find(&products)
-	c.JSON(http.StatusOK, gin.H{"products": products})
+	if err != nil {
+		log.Fatal(err.Error())
+	} else {
+		c.JSON(http.StatusOK, gin.H{"products": products})
+	}
 }
 
 func Show(c *gin.Context) {
@@ -21,17 +24,10 @@ func Show(c *gin.Context) {
 	id := c.Param("id")
 
 	if err := models.DB.First(&product, id).Error; err != nil {
-		switch err {
-		case gorm.ErrRecordNotFound:
-			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Data not found!"})
-			return
-		default:
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-			return
-		}
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Data not found!"})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"product": product})
 	}
-
-	c.JSON(http.StatusOK, gin.H{"product": product})
 }
 
 func Create(c *gin.Context) {
@@ -41,7 +37,6 @@ func Create(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
-
 	models.DB.Create(&product)
 	c.JSON(http.StatusOK, gin.H{"product": product})
 }
@@ -50,38 +45,24 @@ func Update(c *gin.Context) {
 	var product models.Product
 	id := c.Param("id")
 
-	if err := c.ShouldBindJSON(&product); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
-		return
+	if err := models.DB.Model(&product).Where("id = ?", id).First(&product).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Data not found!"})
+	} else {
+		c.ShouldBindJSON(&product)
+		models.DB.Save(&product)
+		c.JSON(http.StatusOK, gin.H{"message": "Data successfully updated!"})
 	}
-
-	if models.DB.Model(&product).Where("id = ?", id).Updates(&product).RowsAffected == 0 {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Cannot update the product"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Data successfully updated!"})
-
 }
 
 func Delete(c *gin.Context) {
 	var product models.Product
+	id := c.Param("id")
 
-	var input struct {
-		Id json.Number
+	if err := models.DB.Model(&product).Where("id = ?", id).First(&product).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Data not found!"})
+	} else {
+		c.ShouldBindJSON(&product)
+		models.DB.Delete(&product)
+		c.JSON(http.StatusOK, gin.H{"message": "Data successfully deleted!"})
 	}
-
-	if err := c.ShouldBindJSON(&input); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
-		return
-	}
-
-	id, _ := input.Id.Int64()
-	if models.DB.Delete(&product, id).RowsAffected == 0 {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Cannot delete the product"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Data successfully deleted!"})
-
 }
