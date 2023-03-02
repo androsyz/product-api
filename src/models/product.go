471aa2beb192862d05462545ad3504469c91e403
Andro Syahreza <androsyahreza3@gmail.com>
2023-03-02 11:12:18 +0700

    add models attribute

--- src/models/product.go
+++ src/models/product.go
@@ -3,5 +3,6 @@ package models
 type Product struct {
 	Id          int64  `gorm:"primaryKey" json:"id"`
 	ProductName string `gorm:"type:varchar(300)" json:"product_name"`
+	Category    string `gorm:"type:varchar(100)" json:"category"`
 	Description string `gorm:"type:text" json:"description"`
 }
